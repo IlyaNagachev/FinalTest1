3f549c0ea9d3376802942676e779187e051017f8
Илья Нагачев <Ilya>
2024-03-01 20:02:10 +0300

    FilterArray count commit

--- Program.cs
+++ Program.cs
@@ -5,4 +5,16 @@ class Program
     static void Main(string[] args){
     
     }
+    static string[] FilterArray(string[] inputArray)
+    {
+        int count = 0;
+
+        foreach (string item in inputArray)
+        {
+            if (item.Length <= 3)
+            {
+                count++;
+            }
+        }
+    }    
 }
